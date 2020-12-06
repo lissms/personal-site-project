c9a0732ee4f8057683810f66b96edc96d3413077
lissms <felissms@gmail.com>
2020-12-07 00:36:42 +0100

    add coming soon contentent

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>My personal website</h1>
+      <h2>Coming soon!</h2>
+    </div>
+  );
+}
+
+export default App;
