e379832475ea47b8c547ecb9bc3e463507bc325b
lissms <felissms@gmail.com>
2020-12-29 23:25:14 +0100

    add data array technologyes

---
+++ src/data/data.js
@@ -0,0 +1,28 @@
+import cssGreenLogo from "../../src/images/css-green.jpg";
+import cssLogo from "../../src/images/css.jpg";
+
+const languages = [
+  {
+    name: "css",
+    greenImageTechnologie: cssGreenLogo,
+    imgeTechnologie: cssLogo,
+    level: "excelent",
+    id: "css1",
+  },
+  {
+    name: "html",
+    greenImageTechnologie: "aqui va la imahgen de background",
+    imgeTechnologie: "aqui el color real de la tecnología",
+    level: "excelent",
+    id: "html2",
+  },
+  {
+    name: "JavaScript",
+    greenImageTechnologie: "aqui va la imahgen de background",
+    imgeTechnologie: "aqui el color real de la tecnología",
+    level: "excelent",
+    id: "Javascript3" /* funcion para poner id, nombre+numero mas 1 */,
+  },
+];
+
+export default languages;
