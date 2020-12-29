5bdbad34d0aaf24f7bf287d5949f8a6f8055a296
lissms <felissms@gmail.com>
2020-12-29 23:23:27 +0100

    add styled for technology  list

---
+++ src/component/skills/technologyList.styled.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const DivContainerLang = styled.div`
+  margin-left: 147px;
+`;
+
+export const TitleLanguages = styled.p`
+  color: #073cb4;
+  font-family: Gugi;
+  font-size: 36px;
+  line-height: 45px;
+  letter-spacing: 0.14em;
+  margin: 0;
+`;
+
+export const UL = styled.ul`
+  list-style: none;
+  display: flex;
+  justify-content: center;
+  padding: 0;
+
+  & li {
+    margin: 0;
+  }
+`;
