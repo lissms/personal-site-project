4d1efadc4dae9819880f88edef1d929a8b434b0c
lissms <felissms@gmail.com>
2021-01-06 16:45:18 +0100

    integrate all new section in App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import SectionAboutMe from "./component/about-me/SectionAboutMe";
 import Header from "./component/header/Header";
+import MyWorks from "./component/my-works/MyWorks";
 import Skills from "./component/skills/Skills";
 import data from "./data/data";
+import Contact from "./component/contact/Contact";
+import Footer from "./component/footer/Footer";
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Header />
       <Skills data={data} />
       <SectionAboutMe data={data} />
+      <MyWorks data={data} />
+      <Contact />
+      <Footer />
     </>
   );
 }
