6ec15bb25f50e670df2e7c17f9af0d1b223d8a6c
lissms <felissms@gmail.com>
2021-01-17 09:16:38 +0100

    add link to my CV

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import cv from "./Frontend_Developer-CV_Lissandra_Mendez_Sanchez.pdf";
 
 function App() {
   return (
     <div className="App">
       <img src={logo} className="App-logo" alt="logo" />
       <h1>My personal website</h1>
+      <a href={cv}>Download my CV</a>
       <h2>Coming soon!</h2>
     </div>
   );
